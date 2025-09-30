4ee72418f8cdd83fcd0c84bfb7e034fd4e60fd81
Hemant Sharma <hemantsharmafli7@gmail.com>
2025-09-30 18:26:34 +0530

    added force-dynamic to avoid nextjs csr warning

--- interviewai-frontend/src/app/interview/session/page.tsx
+++ interviewai-frontend/src/app/interview/session/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import Navbar from "@/components/Navbar";
 import { useEffect, useMemo, useState } from "react";
 import { endInterview, getNextQuestion, getInterview, submitAnswer, runCode } from "@/lib/api";
